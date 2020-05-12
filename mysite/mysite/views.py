6e92ea2536bd8d7f4fe6dad0e85018f8d4f5c7bb
SgMakarov <s.makarov@innopolis.university>
2020-05-12 14:07:52 +0300

    small fix

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,11 +2,6 @@ from django.views import View
 from django.shortcuts import redirect
 
 class AuthView(View):
-    def get(self, request):
-        data = request.data
-        print(data.get("code"))
-        return redirect("/")
-    def post(self, request):
-        data = request.data
-        print(data.get("code"))
+    def get(self, request, *args, **kwargs):
+        print(f"{request}\n{args}\n{kwargs}")
         return redirect("/")

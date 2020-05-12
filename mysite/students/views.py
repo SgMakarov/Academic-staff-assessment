cacc62b39e774188c9f9f42826d7d49b4ad5f33a
SgMakarov <s.makarov@innopolis.university>
2020-05-12 13:51:30 +0300

    small fix

--- mysite/students/views.py
+++ mysite/students/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, redirect
 
 
 def signup(request):
-    return redirect("https://sso.university.innopolis.ru/adfs/oauth2/authorize/?client_id=b04c488e-95c4-47d1-824e-6806731565e5&response_type=code&redirect_uri=https://studprojectfeedback.innopolis.university/login/oauth")
+    return redirect("https://sso.university.innopolis.ru/adfs/oauth2/authorize/?client_id=b04c488e-95c4-47d1-824e-6806731565e5&response_type=code&redirect_uri=https://studprojectfeedback.innopolis.university/login/oauth/&response_mode=form_post")
 

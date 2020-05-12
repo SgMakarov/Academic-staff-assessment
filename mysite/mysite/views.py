d3f31de745cd300a8f1ea76e2738bcd1b45638fd
SgMakarov <s.makarov@innopolis.university>
2020-05-12 14:54:40 +0300

    small fix

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -10,16 +10,15 @@ class AuthView(View):
             "grant_type": "authorization_code",
             "code": request.GET.get('code'),
             "response_type": "code",
-
             "client_id": "b04c488e-95c4-47d1-824e-6806731565e5",
             "client_secret": "JPAHZDIhjh-o1e6XqDBEsnkjx2H_eJI_guJ1iZth",
-
             "redirect_uri": "https://studprojectfeedback.innopolis.university"
         }
+        print(token)
         resp = requests.post("https://sso.university.innopolis.ru/adfs/oauth2/token", data=token, headers={
             'content-type': 'application/json'
         })
-        print(token)
+        
         data = resp.json()
         print(data)
         return redirect("/")

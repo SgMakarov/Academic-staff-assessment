f075974dae1a51c1fce32c89941397aa49024fa4
SgMakarov <s.makarov@innopolis.university>
2020-05-12 14:19:07 +0300

    small fix

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import redirect
 
 class AuthView(View):
     def get(self, request, *args, **kwargs):
-        print(f"{request}\n{args}\n{kwargs}")
+        print(f"{request.GET}\n{args}\n{kwargs}")
         return redirect("/")

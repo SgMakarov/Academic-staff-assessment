2cfdb44ba96b4e7dd307df98fd3b578c030681d4
SgMakarov <s.makarov@innopolis.university>
2020-05-12 14:27:58 +0300

    small fix

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,22 @@
 from django.views import View
 from django.shortcuts import redirect
-
+import requests
 class AuthView(View):
     def get(self, request, *args, **kwargs):
-        print(f"{request.GET}\n{args}\n{kwargs}")
+        token = {
+        "grant_type": "authorization_code",
+        "code": request.GET.get('code'),
+        "response_type": "code",
+
+        "client_id": "b04c488e-95c4-47d1-824e-6806731565e5"
+,
+        "client_secret": "JPAHZDIhjh-o1e6XqDBEsnkjx2H_eJI_guJ1iZth",
+
+        "redirect_uri": "https://studprojectfeedback.innopolis.university"
+    }
+        resp = requests.post("https://sso.university.innopolis.ru/adfs/oauth2/token", data=token, headers={
+            'content-type': 'application/json'
+        })
+        data = resp.json()
+        print(data)
         return redirect("/")

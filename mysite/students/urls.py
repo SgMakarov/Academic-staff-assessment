fa9a49430ddd5ed9c6486aa48303227b42bce532
SgMakarov <s.makarov@innopolis.university>
2020-05-12 13:17:10 +0300

    small fix

--- mysite/students/urls.py
+++ mysite/students/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('register/', views.signup, name='student_registration'),
-    url(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
 ]

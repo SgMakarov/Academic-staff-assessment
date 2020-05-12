96d35da8718bf45175c45e80e056c4e8e40391f1
SgMakarov <s.makarov@innopolis.university>
2020-05-12 13:48:11 +0300

    small fix

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,6 @@
 from django.views import View
 
 class AuthView(View):
-    def post(self, request):
-        print(request.data)
+    def get(self, request):
+        data = request.data
+        print(data.get("code"))
